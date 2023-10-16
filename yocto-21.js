e75aa41e9ebabf2e5a015ca1c2df906845240793
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-10-16 17:42:50 +0200

    Create yocto-21.js

---
+++ yocto-21.js
@@ -0,0 +1,13 @@
+sh-5.0$ bitbake-layers show-layers
+NOTE: Starting bitbake server...
+layer                 path                                      priority
+==========================================================================
+meta                  /home/build/rpi0w/poky-kirkstone/meta     5
+meta-poky             /home/build/rpi0w/poky-kirkstone/meta-poky  5
+meta-yocto-bsp        /home/build/rpi0w/poky-kirkstone/meta-yocto-bsp  5
+meta-oe               /home/build/rpi0w/meta-openembedded/meta-oe  5
+meta-python           /home/build/rpi0w/meta-openembedded/meta-python  5
+meta-multimedia       /home/build/rpi0w/meta-openembedded/meta-multimedia  5
+meta-networking       /home/build/rpi0w/meta-openembedded/meta-networking  5
+meta-raspberrypi      /home/build/rpi0w/meta-raspberrypi        9
+meta-custom-layer     /home/build/rpi0w/meta-custom-layer           6
