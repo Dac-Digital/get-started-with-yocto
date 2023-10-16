e9214144b25b02b70046627b582113000dfb250d
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-10-16 17:35:36 +0200

    Create yocyo-5.js

---
+++ yocyo-5.js
@@ -0,0 +1,6 @@
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers show-layers
