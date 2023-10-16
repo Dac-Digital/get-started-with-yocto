23a36cc97000c82c4d6b8d2ecaae9edd39357e7d
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-10-16 17:39:04 +0200

    Update yocto-9.js

--- yocto-9.js
+++ yocto-9.js
@@ -1,1 +1,19 @@
+Build Configuration:
+BB_VERSION           = "2.0.0"
+BUILD_SYS            = "x86_64-linux"
+NATIVELSBSTRING      = "universal"
+TARGET_SYS           = "arm-poky-linux-gnueabi"
+MACHINE              = "raspberrypi0-wifi"
+DISTRO               = "poky"
+DISTRO_VERSION       = "4.0.10"
+TUNE_FEATURES        = "arm thumb vfp arm1176jzfs callconvention-hard"
+TARGET_FPU           = "hard"
+meta                 
+meta-poky            
+meta-yocto-bsp       = "kirkstone:1e6f147c686a91b9a5996f529e32ee1852f946b7"
+meta-oe              
+meta-python          
+meta-multimedia      
+meta-networking      = "kirkstone:f95484417e3d3e65ca15b460ba71dfd35773f0e4"
+meta-raspberrypi     = "kirkstone:43683cb14b6afc144619335b3a2353b70762ff3e"
 
