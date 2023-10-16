118211175585249280f6cd6041bc99a2de259eea
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-10-16 17:39:33 +0200

    Update yocto-11.js

--- yocto-11.js
+++ yocto-11.js
@@ -1,1 +1,10 @@
-bitbake core-image-base --runonly=fetch
+ctrl_interface=/var/run/wpa_supplicant
+ctrl_interface_group=0
+update_config=1
+
+network={
+        ssid="MYSSID"https://github.com/Dac-Digital/get-started-with-yocto/blob/main/yocto-11.js
+        psk="MY-SECURE-PASSKEY"
+        proto=RSN
+        key_mgmt=WPA-PSK
+}
