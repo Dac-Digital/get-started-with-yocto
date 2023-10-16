495cc9afe52e8de32dbcde093ddac832441e9763
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-10-16 17:34:12 +0200

    Create yocto.js

---
+++ yocto.js
@@ -0,0 +1,3 @@
+git clone --branch kirkstone git://git.yoctoproject.org/poky poky-kirkstone
+git clone --branch kirkstone git://git.yoctoproject.org/meta-raspberrypi.git
+git clone --branch kirkstone git://git.openembedded.org/meta-openembedded
